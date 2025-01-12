4a1a8a16bf4648fc1af3e28e6523285185ac424c
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-01-12 14:34:22 +0100

    feat: add loading ui

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const LoadingScreen = () => {
+    return (
+        <div className='flex min-h-screen items-center justify-center'>
+            <div className='h-32 w-32 animate-spin rounded-full border-t-2 border-gray-900' />
+        </div>
+    )
+}
+
+export default LoadingScreen
